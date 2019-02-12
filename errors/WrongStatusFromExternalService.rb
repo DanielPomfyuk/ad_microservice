33de1823fa4ec5616738d26beeefb20b369b5d88
Your Name <you@example.com>
2019-02-12 16:25:57 +0200

    Exception handling

--- errors/WrongStatusFromExternalService.rb
+++ errors/WrongStatusFromExternalService.rb
@@ -1,6 +1,10 @@
 class WrongStatusFromExternalService < StandardError
   def initialize(message, status)
-    msg = "Something went wrong with the external service. Status is #{status} . The message is #{message}"
-    super(msg+" xujnja")
+    @status = status
+    msg = "Something went wrong with the external service. Status is #{@status} . The message is #{message}"
+    super(msg)
+  end
+  def return_json
+    {:message => self.message,:status => @status}.to_json
   end
 end
--- microservice.rb
+++ microservice.rb
@@ -7,6 +7,7 @@ require 'sinatra/base'
 require 'sinatra/param'
 require_relative 'models/ad'
 require_relative 'errors/WrongStatusFromExternalService'
+require_relative 'errors/DatabaseError'
 set :port, 7777
 set :database, {adapter: "sqlite3", database: "ad.sqlite3"}
 set :show_exceptions, :after_handler
@@ -15,20 +16,26 @@ before do
   content_type 'application/json'
 end
 error Faraday::ConnectionFailed do
-  {:message => env['sinatra.error'].message}.to_json
+  WrongStatusFromExternalService.new(env['sinatra.error'].message,500).return_json
 end
 error WrongStatusFromExternalService do
+  {:message => env['sinatra.error'].message}.to_json
+end
+error DatabaseError do
   {:message => env['sinatra.error'].message}.to_json
 end
 get '/get_user' do
  profile_microservice = Faraday.new(:url=>'http://localhost:3228')
  result = profile_microservice.get('/get_users_info', params = {user_id: 1})
- if result.status != 200
-   puts "WOOOOOOOOOOOOOOOOOOOOT #{result.body}"
-   msg = JSON(result.body)['message']
-   raise WrongStatusFromExternalService.new(msg,result.status)
+ begin
+   if result.status != 200
+     msg = JSON(result.body)['message']
+     raise WrongStatusFromExternalService.new(msg,result.status)
+   end
+   result = JSON.parse(result.body)
+ rescue JSON::ParserError
+   raise WrongStatusFromExternalService.new("Invalid respond type from internal microservice",500)
  end
- result = JSON.parse(result.body)
  fulName = {:name =>result['first_name'],:surname =>result['last_name']}
  fulName.to_json
 end
@@ -37,12 +44,16 @@ post '/add_ad' do
   param :square,   Float, required: true , blank:false
   param :rooms,    Integer, required: true , blank:false
   param :type, String, in: ['house', 'flat', 'apartment', 'residance']
+  begin
   newAd = Ad.new
   newAd.square = params['square']
   newAd.rooms = params['rooms']
   newAd.real_estate_type = params['type']
   newAd.address = params['address']
   create_handler(newAd,'new ad was successfully created')
+  rescue ActiveRecord::StatementInvalid
+    raise DatabaseError.new("Database error",500)
+  end
 end
 def create_handler(model,message)
   result = {}
